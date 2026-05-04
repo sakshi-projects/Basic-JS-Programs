95c8616d8f608f6687c7d02291be097a5c7598e6
Sakshi <sakshisakshi1375@gmail.com>
2026-05-04 14:15:10 +0530

    evenOdd

---
+++ evenOdd.js
@@ -0,0 +1,5 @@
+let a = 19;
+if(a%2==0)
+    console.log("even");
+else
+    console.log("odd");
---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+let a= 0;
+let b =1;
+for(i=0; i<=10;i++)
+{
+    let temp = a+b;
+
+
+    console.log(num);
+}
+
+
