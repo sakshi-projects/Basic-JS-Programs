5956b78c0f504fd5b3ed1c1e3b77756caede908e
Sakshi <sakshisakshi1375@gmail.com>
2026-05-04 14:23:26 +0530

    switch

--- switchCase.js
+++ switchCase.js
@@ -1,2 +1,15 @@
 let a = 10;
 let b = 20;
+let oper = "+"
+switch(oper)
+{
+    case "+":
+        console.log(a+b);
+    case "-":
+        console.log(a-b);
+    case "*":
+        console.log(a*b);
+    case "/":
+        console.log(a/b);
+
+}
