547884d6a1e92d18d3700dfc5ed331321c32fc42
Sakshi <sakshisakshi1375@gmail.com>
2026-05-04 14:36:22 +0530

    switch

---
+++ switchCase.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 20;
+let oper = "+"
+switch(oper)
+{
+    case "+":
+        console.log(a+b);
+        break;
+    case "-":
+        console.log(a-b);
+        break;
+
+    case "*":
+        console.log(a*b);
+        break;
+    case "/":
+        console.log(a/b);
+
+}
---
+++ weekDays.js
@@ -0,0 +1,27 @@
+let day = 5;
+switch(day)
+{
+    case 1:
+        console.log("sunday");
+        break;
+    case 2:
+        console.log("monday");
+        break;
+
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+
+
+}
